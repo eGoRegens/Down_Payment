b32f2a7d55069a39afb7a51bb09699866265bfa9
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 21:59:00 +0400

    добавление неизменяемых переменных

--- src/main/java/Appartment.java
+++ src/main/java/Appartment.java
@@ -1,12 +1,12 @@
 public class Appartment {
     public static void main(String[] args) {
-        int apartmentPrice = 8900000;
-        double downPaymentPercentage = 15.5;
-        double downPaymentAmount = (apartmentPrice * downPaymentPercentage) / 100;  // сумма первоначального взноса
+        final int Apartment_Price = 8900000;
+        final double Down_Payment_Percentage = 15.5;
+        double downPaymentAmount = (Apartment_Price * Down_Payment_Percentage) / 100;  // сумма первоначального взноса
         int downPaymentInteger = (int) downPaymentAmount;  //округление до целочисленного
 
-        System.out.println("Стоимость квартиры составляет " + apartmentPrice + " у.е.");
-        System.out.println("Требуемый процент первоначального взноса " + downPaymentPercentage + "% от стоимости квартиры.");
+        System.out.println("Стоимость квартиры составляет " + Apartment_Price + " у.е.");
+        System.out.println("Требуемый процент первоначального взноса " + Down_Payment_Percentage + "% от стоимости квартиры.");
         System.out.println("Сумма первоначального взноса составит = " + downPaymentInteger + " у.е.");
     }
 }
