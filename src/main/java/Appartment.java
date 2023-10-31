933ebf016ce829048c9433170030229e6fbcec25
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-31 10:44:05 +0400

    добавление констант

--- src/main/java/Appartment.java
+++ src/main/java/Appartment.java
@@ -1,12 +1,12 @@
 public class Appartment {
     public static void main(String[] args) {
-        final int Apartment_Price = 8900000;
-        final double Down_Payment_Percentage = 15.5;
-        double downPaymentAmount = (Apartment_Price * Down_Payment_Percentage) / 100;  // сумма первоначального взноса
-        int downPaymentInteger = (int) downPaymentAmount;  //округление до целочисленного
+        final int APARTMENT_PRICE = 8900000;
+        final double DOWN_PAYMENT = 15.5;
+        final double DOWN_PAYMENT_AMOUNT = (APARTMENT_PRICE * DOWN_PAYMENT) / 100;  // сумма первоначального взноса
+        final int DOWN_PAYMENT_INTEGER = (int) DOWN_PAYMENT_AMOUNT;  //округление до целочисленного
 
-        System.out.println("Стоимость квартиры составляет " + Apartment_Price + " у.е.");
-        System.out.println("Требуемый процент первоначального взноса " + Down_Payment_Percentage + "% от стоимости квартиры.");
-        System.out.println("Сумма первоначального взноса составит = " + downPaymentInteger + " у.е.");
+        System.out.println("Стоимость квартиры составляет " + APARTMENT_PRICE + " у.е.");
+        System.out.println("Требуемый процент первоначального взноса " + DOWN_PAYMENT + "% от стоимости квартиры.");
+        System.out.println("Сумма первоначального взноса составит = " + DOWN_PAYMENT_INTEGER + " у.е.");
     }
 }
