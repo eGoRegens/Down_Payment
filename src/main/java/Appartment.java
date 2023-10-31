50d58a50c44dc175563220111ac14eaa760d915a
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-31 19:49:20 +0400

    переименование переменных

--- src/main/java/Appartment.java
+++ src/main/java/Appartment.java
@@ -1,12 +1,12 @@
 public class Appartment {
     public static void main(String[] args) {
-        final int APARTMENT_PRICE = 8900000;
-        final double DOWN_PAYMENT = 15.5;
-        final double DOWN_PAYMENT_AMOUNT = (APARTMENT_PRICE * DOWN_PAYMENT) / 100;  // сумма первоначального взноса
-        final int DOWN_PAYMENT_INTEGER = (int) DOWN_PAYMENT_AMOUNT;  //округление до целочисленного
+        final int apartmentPrice = 8900000;
+        final double downPaymentPercentage = 15.5;
+        final double downPaymentAmount = (apartmentPrice * downPaymentPercentage) / 100;  // сумма первоначального взноса
+        final int downPaymentInteger = (int) downPaymentAmount;  //округление до целочисленного
 
-        System.out.println("Стоимость квартиры составляет " + APARTMENT_PRICE + " у.е.");
-        System.out.println("Требуемый процент первоначального взноса " + DOWN_PAYMENT + "% от стоимости квартиры.");
-        System.out.println("Сумма первоначального взноса составит = " + DOWN_PAYMENT_INTEGER + " у.е.");
+        System.out.println("Стоимость квартиры составляет " + apartmentPrice + " у.е.");
+        System.out.println("Требуемый процент первоначального взноса " + downPaymentPercentage + "% от стоимости квартиры.");
+        System.out.println("Сумма первоначального взноса составит = " + downPaymentInteger + " у.е.");
     }
 }
